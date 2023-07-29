be8377d904f034ec5ff7b554e1ff56dbcd2e2dc3
Nataliia4444 <127755653+Nataliia4444@users.noreply.github.com>
2023-07-29 10:16:21 +0300

    add liberty

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const preview = galleryItems
+  .map(
+    (img) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href=${img.original}>
+            <img
+            class="gallery__image"
+            src= ${img.preview}
+            data-source= ${img.original}
+            alt= ${img.description}
+            />
+        </a>
+    </li>`
+  )
+  .join("");
+const image = preview;
+gallery.innerHTML = image;
+gallery.addEventListener("click", onClick);
+function onClick(event) {
+  console.log(event.target);
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src=${event.target.dataset.source} width="800" height="600">
+`);
+
+  instance.show();
+
+  gallery.addEventListener("keydown", onKey);
+  function onKey(event) {
+    if (event.key === "Escape") {
+      instance.close();
+      console.log(99999);
+    }
+
+    //   instance.show();
+  }
+}
